a365cffb585d4c17666f86f41bb2e92f2cab88a2
Derek Ellis <derekellis170@gmail.com>
2023-07-23 17:18:28 -0400

    Remove ksp and kotlin-inject

--- gtfs/build.gradle.kts
+++ gtfs/build.gradle.kts
@@ -4,24 +4,17 @@ import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.publishing)
 }
 
 kotlin {
     explicitApi()
-
-    sourceSets.main {
-        kotlin.srcDir("$buildDir/generated/ksp/main/kotlin")
-    }
 }
 
 dependencies {
     api(libs.exposed.core)
 
-    ksp(libs.kotlin.inject.compiler)
     implementation(libs.okhttp)
-    implementation(libs.kotlin.inject.runtime)
     implementation(libs.bundles.ktor.client)
     implementation(libs.bundles.sqldelight)
     implementation(libs.csv)
