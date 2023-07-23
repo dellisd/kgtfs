bc8f0b8a4d2f0e011514c6d02fd01d31c2c36ee2
Derek Ellis <derekellis170@gmail.com>
2023-07-23 17:22:34 -0400

    Fix rc02 compatibility

--- raptor/src/main/kotlin/ca/derekellis/kgtfs/raptor/db/QueryHelper.kt
+++ raptor/src/main/kotlin/ca/derekellis/kgtfs/raptor/db/QueryHelper.kt
@@ -1,9 +1,10 @@
 package ca.derekellis.kgtfs.raptor.db
 
 import app.cash.sqldelight.ExecutableQuery
+import app.cash.sqldelight.db.QueryResult
 
 internal fun <T : Any> ExecutableQuery<T>.executeAsSet(): Set<T> = execute { cursor ->
     val result = mutableSetOf<T>()
-    while (cursor.next()) result.add(mapper(cursor))
-    result
+    while (cursor.next().value) result.add(mapper(cursor))
+    QueryResult.Value(result)
 }.value
